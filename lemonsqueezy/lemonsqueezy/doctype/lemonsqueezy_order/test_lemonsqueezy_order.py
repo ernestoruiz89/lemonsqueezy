977f02901181ca3bc5e6207f61839a31b22d5ce0
Ernesto Ruiz Escorcia <eruiz@wbapps.com>
2025-11-26 22:43:33 +0000

    fix: select fields data

---
+++ lemonsqueezy/lemonsqueezy/doctype/lemonsqueezy_order/test_lemonsqueezy_order.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Ernesto Ruiz and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestLemonSqueezyOrder(FrappeTestCase):
+	pass
